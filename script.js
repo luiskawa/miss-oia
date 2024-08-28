4726bb22eae2ad14ba43bf624a36ab7d57a7f268
luiskawa <126871592+luiskawa@users.noreply.github.com>
2024-08-28 07:40:21 -0300

    a

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-const caixaPricipal - document.querySelector(".caixa-principal");
-const caixa - document.querySelector(".caixa-principal");
+const caixaPricipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlterntivas = document.querySelectorAl9l(".caixa-alternativas")
+const caixaResultado = document.querySelectorAll(".caixa-resultado")
+const textoResultado = document.querySelectorAll(".texto-resultado")
